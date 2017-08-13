8da381f7ef16483b97a3ea8b6bdcc6376b736e07
Loc Le <lehuuloc.qt@gmail.com>
2017-08-13 08:54:19 +0700

    Initial commit

---
+++ src/main/java/botay/Application.java
@@ -0,0 +1,23 @@
+/**
+ * Application class
+ * 
+ * Version 1.0
+ */
+package botay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+
+/**
+ * Application provide the entry of application
+ * 
+ * @author tamvn.ax
+ *
+ */
+@SpringBootApplication()
+public class Application {
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+}
