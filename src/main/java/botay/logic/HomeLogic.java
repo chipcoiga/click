be5333fcdd220ad273657e634744bb5bb57f41f6
Loc Le <lehuuloc.qt@gmail.com>
2017-08-13 09:04:21 +0700

    dev branch

---
+++ src/main/java/botay/logic/HomeLogic.java
@@ -0,0 +1,6 @@
+package botay.logic;
+
+public interface HomeLogic
+{
+    String login(String username, String password);
+}
---
+++ src/main/java/botay/logic/impl/HomLoginImpl.java
@@ -0,0 +1,11 @@
+package botay.logic.impl;
+
+import botay.logic.HomeLogic;
+
+public class HomLoginImpl implements HomeLogic{
+
+    @Override
+    public String login(String username, String password) {
+        return null;
+    }
+}
